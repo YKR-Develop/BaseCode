587e76d21fe4f8aec61e6571b5ce8ac896fca076
yukuro-portfolio <yukuro.createfolio.gmail.com>
2024-04-05 20:44:49 +0900

    ベースコーディング作成

--- js/common.js
+++ js/common.js
@@ -4,5 +4,31 @@
 ================================================== */
 
 /* ==================================================
- 共通ヘッダー
+ ヘッダーをHTMLに出力
 ================================================== */
+const includeHeader = new XMLHttpRequest();
+includeHeader.open("GET", "include/header.html", true);
+includeHeader.onreadystatechange = function() {
+  if (includeHeader.readyState === 4 && includeHeader.status === 200) {
+    const headerHTML = includeHeader.responseText;
+    // 以下 header.htmlを処理するためのコード
+    const header = document.querySelector('#js-header');
+    header.insertAdjacentHTML("afterbegin", headerHTML);
+  }
+};
+includeHeader.send();
+
+/* ==================================================
+ フッターをHTMLに出力
+================================================== */
+const includeFooter = new XMLHttpRequest();
+includeFooter.open("GET", "include/footer.html", true);
+includeFooter.onreadystatechange = function() {
+  if (includeFooter.readyState === 4 && includeFooter.status === 200) {
+    const footerHTML = includeFooter.responseText;
+    // 以下 footer.htmlを処理するためのコード
+    const footer = document.querySelector('#js-footer');
+    footer.insertAdjacentHTML("afterbegin", footerHTML);
+  }
+};
+includeFooter.send();
